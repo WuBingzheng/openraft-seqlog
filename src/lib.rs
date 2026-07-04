086675cb0a83cfca5e1c41f088c20e0e389245e3
wub <wubingzheng@gmail.com>
2026-07-04 15:17:47 +0800

    grow

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,19 @@
 use std::io;
 use std::marker::PhantomData;
-
-use openraft::OptionalSync;
-// use serde::Serialize;
+use std::path::Path;
 
 use openraft::alias::{LogIdOf, VoteOf};
 use openraft::storage::{IOFlushed, RaftLogStorage};
-use openraft::{LogState, OptionalSend, RaftLogReader, RaftTypeConfig};
+use openraft::{LogState, OptionalSend, OptionalSync, RaftLogReader, RaftTypeConfig};
 
-use seqlog::{SeqLog, SeqLogReader, SeqLogSyncer};
+use seqlog::{Error, SeqLog, SeqLogReader, SeqLogSyncer};
 
-pub struct SeqLogStore<C, Codec>
+pub struct SeqLogStore<C>
 where
     C: RaftTypeConfig,
-    Codec: EntryCodec<C::Entry>,
+    C::Entry: EntryCodec,
 {
     store: SeqLog,
-    codec: Codec,
     write_buf: Vec<u8>,
     write_pos: Vec<usize>,
     _p: PhantomData<C>,
@@ -25,22 +22,35 @@ where
 pub struct Reader<C>
 where
     C: RaftTypeConfig,
+    C::Entry: EntryCodec,
 {
     inner: SeqLogReader,
     _p: PhantomData<C>,
 }
 
-pub trait EntryCodec<E>: Send + OptionalSync + 'static {
-    type Error;
+pub trait EntryCodec: Sized + Send + OptionalSync + 'static {
+    type EncodeError: std::fmt::Debug;
+    type DecodeError: std::fmt::Debug + Send + Sync + 'static + std::error::Error;
 
-    fn encode(&self, entry: &E, buf: &mut Vec<u8>) -> Result<(), Self::Error>;
+    fn encode(&self, buf: &mut Vec<u8>) -> Result<(), Self::EncodeError>;
 
-    fn decode(&self, data: &[u8]) -> Result<E, Self::Error>;
+    fn decode(data: &[u8]) -> Result<Self, Self::DecodeError>;
+}
+
+fn seq_log_err(from: Error) -> std::io::Error {
+    match from {
+        Error::Io(err) => err,
+        Error::EntryTooLarge(_) | Error::SeqPurged(_, _) | Error::SeqNotReached(_, _) => {
+            std::io::Error::new(std::io::ErrorKind::InvalidInput, from)
+        }
+        _ => std::io::Error::new(std::io::ErrorKind::InvalidData, from),
+    }
 }
 
 impl<C> RaftLogReader<C> for Reader<C>
 where
     C: RaftTypeConfig,
+    C::Entry: EntryCodec,
 {
     async fn try_get_log_entries<
         RB: std::ops::RangeBounds<u64> + Clone + std::fmt::Debug + OptionalSend,
@@ -48,7 +58,34 @@ where
         &mut self,
         range: RB,
     ) -> Result<Vec<<C as RaftTypeConfig>::Entry>, std::io::Error> {
-        todo!()
+        let start = match range.start_bound() {
+            std::ops::Bound::Included(index) => *index,
+            std::ops::Bound::Excluded(index) => *index + 1,
+            std::ops::Bound::Unbounded => {
+                return Err(std::io::Error::from(std::io::ErrorKind::InvalidInput));
+            }
+        };
+        if self.inner.next_seq() != start {
+            // This is slow.
+            self.inner.reset(start).map_err(seq_log_err)?;
+        }
+
+        let end = match range.end_bound() {
+            std::ops::Bound::Included(index) => *index + 1,
+            std::ops::Bound::Excluded(index) => *index,
+            std::ops::Bound::Unbounded => u64::MAX,
+        };
+
+        let mut res = Vec::new();
+        for _ in start..end {
+            let Some(buf) = self.inner.next().map_err(seq_log_err)? else {
+                break;
+            };
+            let entry = C::Entry::decode(buf)
+                .map_err(|err| std::io::Error::new(std::io::ErrorKind::InvalidData, err))?;
+            res.push(entry);
+        }
+        Ok(res)
     }
 
     async fn read_vote(
@@ -58,10 +95,10 @@ where
     }
 }
 
-impl<C, Codec> RaftLogStorage<C> for SeqLogStore<C, Codec>
+impl<C> RaftLogStorage<C> for SeqLogStore<C>
 where
     C: RaftTypeConfig,
-    Codec: EntryCodec<C::Entry>,
+    C::Entry: EntryCodec,
 {
     type LogReader = Reader<C>;
 
@@ -121,7 +158,7 @@ where
         self.write_pos.push(0);
 
         for entry in entries.into_iter() {
-            let _ = self.codec.encode(&entry, &mut self.write_buf);
+            let _ = entry.encode(&mut self.write_buf);
             self.write_pos.push(self.write_buf.len());
         }
 
@@ -146,7 +183,11 @@ where
     ///   transaction, but it must not leave a **hole** in logs. In other words, a non-transactional
     ///   truncation removes log entries from the end backward to this `last_log_id`.
     async fn truncate_after(&mut self, last_log_id: Option<LogIdOf<C>>) -> Result<(), io::Error> {
-        todo!()
+        let index = match last_log_id {
+            Some(log_id) => log_id.index(),
+            None => 0,
+        };
+        self.store.truncate(index).map_err(seq_log_err)
     }
 
     /// Purge logs up to `log_id`, inclusive
@@ -155,6 +196,17 @@ where
     ///
     /// - It must not leave a **hole** in logs.
     async fn purge(&mut self, log_id: LogIdOf<C>) -> Result<(), io::Error> {
-        todo!()
+        self.store.purge(log_id.index()).map_err(seq_log_err)
+    }
+}
+
+impl<C> SeqLogStore<C>
+where
+    C: RaftTypeConfig,
+    C::Entry: EntryCodec,
+{
+    pub fn create<P: AsRef<Path>>(path: P) -> Result<Self, io::Error> {
+        SeqLog::create(path, start_seq).map_err(seq_log_err)?;
+        Ok(())
     }
 }
