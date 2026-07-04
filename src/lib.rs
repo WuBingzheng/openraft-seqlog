8a8190bb89d6183cad86b2a9c0c88b53c5280212
wub <wubingzheng@gmail.com>
2026-07-04 11:20:39 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,160 @@
+use std::io;
+use std::marker::PhantomData;
+
+use openraft::OptionalSync;
+// use serde::Serialize;
+
+use openraft::alias::{LogIdOf, VoteOf};
+use openraft::storage::{IOFlushed, RaftLogStorage};
+use openraft::{LogState, OptionalSend, RaftLogReader, RaftTypeConfig};
+
+use seqlog::{SeqLog, SeqLogReader, SeqLogSyncer};
+
+pub struct SeqLogStore<C, Codec>
+where
+    C: RaftTypeConfig,
+    Codec: EntryCodec<C::Entry>,
+{
+    store: SeqLog,
+    codec: Codec,
+    write_buf: Vec<u8>,
+    write_pos: Vec<usize>,
+    _p: PhantomData<C>,
+}
+
+pub struct Reader<C>
+where
+    C: RaftTypeConfig,
+{
+    inner: SeqLogReader,
+    _p: PhantomData<C>,
+}
+
+pub trait EntryCodec<E>: Send + OptionalSync + 'static {
+    type Error;
+
+    fn encode(&self, entry: &E, buf: &mut Vec<u8>) -> Result<(), Self::Error>;
+
+    fn decode(&self, data: &[u8]) -> Result<E, Self::Error>;
+}
+
+impl<C> RaftLogReader<C> for Reader<C>
+where
+    C: RaftTypeConfig,
+{
+    async fn try_get_log_entries<
+        RB: std::ops::RangeBounds<u64> + Clone + std::fmt::Debug + OptionalSend,
+    >(
+        &mut self,
+        range: RB,
+    ) -> Result<Vec<<C as RaftTypeConfig>::Entry>, std::io::Error> {
+        todo!()
+    }
+
+    async fn read_vote(
+        &mut self,
+    ) -> Result<Option<openraft::type_config::alias::VoteOf<C>>, std::io::Error> {
+        todo!()
+    }
+}
+
+impl<C, Codec> RaftLogStorage<C> for SeqLogStore<C, Codec>
+where
+    C: RaftTypeConfig,
+    Codec: EntryCodec<C::Entry>,
+{
+    type LogReader = Reader<C>;
+
+    /// Returns the last deleted log id and the last log id.
+    ///
+    /// The impl should **not** consider the applied log id in state machine.
+    /// The returned `last_log_id` could be the log id of the last present log entry, or the
+    /// `last_purged_log_id` if there is no entry at all.
+    // NOTE: This can be made into sync, provided all state machines will use atomic read or the
+    // like.
+    async fn get_log_state(&mut self) -> Result<LogState<C>, io::Error> {
+        todo!()
+    }
+
+    async fn get_log_reader(&mut self) -> Self::LogReader {
+        let inner = self.store.reader(self.store.sync_seq(), true).unwrap();
+        Reader {
+            inner,
+            _p: PhantomData::default(),
+        }
+    }
+
+    /// Save vote to storage.
+    ///
+    /// ### To ensure correctness:
+    ///
+    /// The vote must be persisted on disk before returning.
+    async fn save_vote(&mut self, vote: &VoteOf<C>) -> Result<(), io::Error> {
+        todo!()
+    }
+
+    /// Append log entries and call the `callback` once logs are persisted on disk.
+    ///
+    /// It should return immediately after saving the input log entries in memory and calls the
+    /// `callback` when the entries are persisted on disk, i.e., avoid blocking.
+    ///
+    /// This method is still async because preparing the IO is usually async.
+    ///
+    /// ### To ensure correctness:
+    ///
+    /// - When this method returns, the entries must be readable, i.e., a `LogReader` can read these
+    ///   entries.
+    ///
+    /// - When the `callback` is called, the entries must be persisted on disk.
+    ///
+    ///   NOTE that: the `callback` can be called either before or after this method returns.
+    ///
+    /// - There must not be a **hole** in logs. Because Raft only examines the last log id to ensure
+    ///   correctness.
+    async fn append<I>(&mut self, entries: I, callback: IOFlushed<C>) -> Result<(), io::Error>
+    where
+        I: IntoIterator<Item = C::Entry> + OptionalSend,
+        I::IntoIter: OptionalSend,
+    {
+        self.write_buf.clear();
+        self.write_pos.clear();
+        self.write_pos.push(0);
+
+        for entry in entries.into_iter() {
+            let _ = self.codec.encode(&entry, &mut self.write_buf);
+            self.write_pos.push(self.write_buf.len());
+        }
+
+        let inputs: Vec<_> = self
+            .write_pos
+            .windows(2)
+            .map(|i| &self.write_buf[i[0]..i[1]])
+            .collect();
+
+        self.store.append(&inputs).unwrap();
+
+        self.store.sync().unwrap();
+        callback.io_completed(Ok(()));
+        Ok(())
+    }
+
+    /// Truncate logs after `last_log_id`, exclusive
+    ///
+    /// ### To ensure correctness:
+    ///
+    /// - It must not leave a **hole** in logs: It is OK if the truncation is not done in
+    ///   transaction, but it must not leave a **hole** in logs. In other words, a non-transactional
+    ///   truncation removes log entries from the end backward to this `last_log_id`.
+    async fn truncate_after(&mut self, last_log_id: Option<LogIdOf<C>>) -> Result<(), io::Error> {
+        todo!()
+    }
+
+    /// Purge logs up to `log_id`, inclusive
+    ///
+    /// ### To ensure correctness:
+    ///
+    /// - It must not leave a **hole** in logs.
+    async fn purge(&mut self, log_id: LogIdOf<C>) -> Result<(), io::Error> {
+        todo!()
+    }
+}
